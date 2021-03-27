991fcea61d41848c7521f9482866ea5939442d0f
abrar-nazib@github.com <abrarnazib@gmail.com>
2021-03-27 15:42:09 +0600

    SCAN

--- c++/codejam/reversort.cpp
+++ c++/codejam/reversort.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
 #include <cstring>
 #include <stdio.h>
-void scan_std();
+
 int main()
 {
-    scan_std();
-    return 0;
-}
-
-void scan_std()
-{
-    int N, A[N];
-    printf("Input no of element in array A: ");
-    scanf("%d", &N);
-    printf("You entered: %d\n", N);
-    printf("Input array A elements in one line: ");
-    for (int i = 0; i < N; i++)
+    int T = 0;
+    scanf("%d", &T);
+    for (int t = 0; t < T; t++)
     {
-        fscanf(stdin, "%d", &A[i]);
-        printf("A[%d] is: %d\n", i, A[i]);
+        //scanning part of the Problem
+        int N;
+        printf("Input no of element in array A: ");
+        scanf("%d", &N);
+        int A[N];
+        printf("You entered: %d\n", N);
+        printf("Input array A elements in one line: ");
+        for (int i = 0; i < N; i++)
+        {
+            fscanf(stdin, "%d", &A[i]);
+        }
+        for (int xx = 0; xx < N; xx++)
+        {
+            printf("A[%d] is: %d\n", xx, A[xx]);
+        }
     }
+
+    return 0;
 }
